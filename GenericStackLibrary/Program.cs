37ab0e17261fd2ccb9759e3dafc210d232b0d3a1
Justin Johnson <johnsonj561@gmail.com>
2016-12-04 09:18:33 -0500

    Add project files.

---
+++ GenericStackLibrary/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericStackLibrary {
+    class Program {
+        static void Main(string[] args) {
+        }
+    }
+}
